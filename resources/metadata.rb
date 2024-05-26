f5165e25f186d5ec4e0f59b693a47437258fd740
Miguel Negrón <45871721+manegron@users.noreply.github.com>
2024-05-26 09:58:02 +0100

    Update metadata.rb

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -1,7 +1,6 @@
 name             'n2klocd'
-maintainer       'redborder'
-maintainer_email 'eareyes@redborder.com'
-license          'All rights reserved'
+maintainer       'Eneo Tecnología S.L.'
+maintainer_email 'git@redborder.com'
+license          'AGPL-3.0'
 description      'Installs/Configures cookbook-n2klocd'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.2'
